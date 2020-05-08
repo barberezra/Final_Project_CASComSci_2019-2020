e03c31a59fcdc2243d39feb3466018c913588254
Ezra B <santmonbarb@gmail.com>
2020-05-08 08:18:23 -0700

    Made an app template to play with different ideas

---
+++ FinalProjectApp/FinalProjectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProjectApp
+//
+//  Created by Ezra Barber on 5/8/20.
+//  Copyright © 2020 Ezra Barber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

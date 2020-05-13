7ee818ab931d9da1064cc374c3b65d2bc37c18c5
Ezra B <santmonbarb@gmail.com>
2020-05-13 08:19:30 -0700

    Updated FinalApp
    Have a bug where film types are undeclared - getting back into API calls

--- FinalProjectApp/FinalProjectApp/ViewController.swift
+++ FinalProjectApp/FinalProjectApp/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+   func fetchFilms(completionHandler: @escaping ([Film]) -> Void) {
+     let url = URL(string: domainUrlString + "films/")!
 
+     let task  URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+       if let error = error {
+         print("Error with fetching films: \(error)")
+         return
+       }
+       
+       guard let httpResponse = response as? HTTPURLResponse,
+             (200...299).contains(httpResponse.statusCode) else {
+         print("Error with the response, unexpected status code: \(response)")
+         return
+       }
 
+       if let data = data,
+         let filmSummary = try? JSONDecoder().decode(FilmSummary.self, from: data) {
+         completionHandler(filmSummary.results ?? [])
+       }
+     })
+     task.resume()
+   }
+    
 }
 

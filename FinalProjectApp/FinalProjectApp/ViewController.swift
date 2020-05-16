00018c2d9f84111b63cdaba668a1b5e2f92ef962
Ezra B <santmonbarb@gmail.com>
2020-05-16 11:31:52 -0700

    made API fetch pentatonix picture
    Added a task to fetch a picture from a certain URL

--- FinalProjectApp/FinalProjectApp/ViewController.swift
+++ FinalProjectApp/FinalProjectApp/ViewController.swift
@@ -1,43 +1,21 @@
-//
-//  ViewController.swift
-//  FinalProjectApp
-//
-//  Created by Ezra Barber on 5/8/20.
-//  Copyright © 2020 Ezra Barber. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var resultsLabel: UILabel!
     
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var imageURL = URL(string: "https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_standard/public/images/19/07/ptx.jpg?itok=Nmbx3Ivy")!
+        let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
+            if error == nil {
+                let loadedImage = UIImage(data: data!)
+                self.imageView.image = loadedImage!
+            }
+        }
+        task.resume()
+        
     }
-   func fetchFilms(completionHandler: @escaping ([Film]) -> Void) {
-     let url = URL(string: domainUrlString + "films/")!
-
-     let task  URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-       if let error = error {
-         print("Error with fetching films: \(error)")
-         return
-       }
-       
-       guard let httpResponse = response as? HTTPURLResponse,
-             (200...299).contains(httpResponse.statusCode) else {
-         print("Error with the response, unexpected status code: \(response)")
-         return
-       }
-
-       if let data = data,
-         let filmSummary = try? JSONDecoder().decode(FilmSummary.self, from: data) {
-         completionHandler(filmSummary.results ?? [])
-       }
-     })
-     task.resume()
-   }
     
-}
 
+}

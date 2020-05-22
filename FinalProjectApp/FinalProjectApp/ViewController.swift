dcaa47d091a7561a1710ae911dac1d017f477400
Ezra B <santmonbarb@gmail.com>
2020-05-21 18:35:39 -0700

    Added SWAPI support
    Changed URL to swapi and hard-coded planets dictionary to it.

--- FinalProjectApp/FinalProjectApp/ViewController.swift
+++ FinalProjectApp/FinalProjectApp/ViewController.swift
@@ -2,20 +2,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelView: UILabel!
+    @IBOutlet weak var findButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var imageURL = URL(string: "https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_standard/public/images/19/07/ptx.jpg?itok=Nmbx3Ivy")!
-        let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
-            if error == nil {
-                let loadedImage = UIImage(data: data!)
-                self.imageView.image = loadedImage!
-            }
-        }
-        task.resume()
         
     }
     
-
+    struct Response: Codable {
+        let name: String
+    }
+    
+    @IBAction func grab() {
+        let apiURL = URL(string: "https://swapi.dev/api/planets/12/")!
+        let task = URLSession.shared.dataTask(with: apiURL) { (data, response, error) in
+            if let data = data {
+                do { let dictionary = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                    print(dictionary)
+                    let res = try JSONDecoder().decode(Response.self, from: data)
+                    let name = res.name
+                    DispatchQueue.main.async() {
+                    self.labelView.text = String(res.name)
+                    }
+            }
+                catch {
+                    print("This errored out for some reason :(")
+                }
+            }
+        }
+        task.resume()
+    }
 }
+
